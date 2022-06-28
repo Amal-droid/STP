28c7de44d5e8ebc1af2c310317a4b35d5fbea15b
Amal-droid <amalpp42@gmail.com>
2022-06-28 18:46:46 +0530

    commit

--- studentportalapp/serializer.py
+++ studentportalapp/serializer.py
@@ -1,25 +1,39 @@
-from asyncore import read
-import numbers
-from turtle import title
-from venv import create
+# from rest_framework import serializers
+# from studentportalapp.models import Book
+
+# class BookSerializer(serializers.Serializer):
+#     id =serializers.IntegerField(read_only=True)
+#     title =serializers.CharField()
+#     number_of_pages = serializers.IntegerField()
+#     publish_date=serializers.DateField()
+#     quantity=serializers.IntegerField()
+
+#     def create(self, data):
+#         return Book.objects.create(**data) 
+
+#     def update(self,instance,data):
+#         instance.title=data.get('title',instance.title)
+#         instance.number_of_pages=data.get('number_of_pages',instance.number_of_pages)
+#         instance.publish_date=data.get('publish_date',instance.publish_date)
+#         instance.quantity=data.get('quantity',instance.quantity)
+        
+#         instance.save()
+#         return instance
+from django.forms import ValidationError
 from rest_framework import serializers
 from studentportalapp.models import Book
 
-class BookSerializer(serializers.Serializer):
-    id =serializers.IntegerField(read_only=True)
-    title =serializers.CharField()
-    number_of_pages = serializers.IntegerField()
-    publish_date=serializers.DateField()
-    quantity=serializers.IntegerField()
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Book
+        fields="__all__"
 
-    def create(self, data):
-        return Book.objects.create(**data) 
+    def validate_title(self, value):
+        if  value =="violance":
+            raise ValidationError("No violance please")
+        return value    
 
-    def update(self,instance,data):
-        instance.title=data.get('title',instance.title)
-        instance.number_of_pages=data.get('number_of_pages',instance.number_of_pages)
-        instance.publish_date=data.get('publish_date',instance.publish_date)
-        instance.quantity=data.get('quantity',instance.quantity)
-        
-        instance.save()
-        return instance
+    def validate(self,data):
+          if data["number_of_pages"]>200 and data["quantity"]>600:
+            raise ValidationError("too heavy")
+          return data
--- studentportalapp/urls.py
+++ studentportalapp/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
-from studentportalapp.views import book_list,book_create,book
+# from django.urls import path
+# from studentportalapp.views import book_list,book_create,book
 
+# urlpatterns=[
+#     path('',book_create),
+#     path('list/',book_list),
+#     path('<int:pk>',book)
+#     ]
+
+from django.urls import path
+from studentportalapp.views import BookCreate, BookList,BookDetails
 urlpatterns=[
-    path('',book_create),
-    path('list/',book_list),
-    path('<int:pk>',book)
+    path('list/',BookList.as_view()),
+    path('',BookCreate.as_view()),
+    path('<int:pk>',BookDetails.as_view()) 
     ]
